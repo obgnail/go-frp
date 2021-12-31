dd902f20125687aed695a3218c92b85b80da1df4
HeyingliangFrontEnd <heyingliang@ones.ai>
2021-12-31 17:03:28 +0800

    fin frps roughly

--- connection/conn.go
+++ connection/conn.go
@@ -14,7 +14,9 @@ import (
 )
 
 const (
-	BufferEndFlag = '\n'
+	BufferRequestStartFlag  = '1'
+	BufferResponseStartFlag = '2'
+	BufferEndFlag           = '\n'
 )
 
 type Conn struct {
@@ -32,12 +34,34 @@ func NewConn(tcpConn *net.TCPConn) *Conn {
 		Reader:      bufio.NewReader(tcpConn),
 		ContextChan: make(chan *context.Context, 1024),
 	}
+	//go c.accept()
 	return c
 }
 
-func (c *Conn) Write(req *context.Request) (err error) {
-	buf, _ := json.Marshal(req)
-	buffer := bytes.NewBuffer(buf)
+//func (c *Conn) accept() {
+//	for {
+//		ctx := &context.Context{}
+//		req, err := c.Read()
+//		if err != nil {
+//			err = errors.Trace(err)
+//			fmt.Println("[WARN] accept msg err:", err)
+//			continue
+//		}
+//		ctx.SetRequest(req)
+//		c.ContextChan <- ctx
+//	}
+//}
+
+func (c *Conn) read() (buff []byte, err error) {
+	buff, err = c.Reader.ReadBytes(BufferEndFlag)
+	if err == io.EOF {
+		c.closeFlag = true
+	}
+	return buff, err
+}
+
+func (c *Conn) Write(buff []byte) (err error) {
+	buffer := bytes.NewBuffer(buff)
 	buffer.WriteByte(BufferEndFlag)
 	_, err = c.TcpConn.Write(buffer.Bytes())
 	if err != nil {
@@ -47,23 +71,26 @@ func (c *Conn) Write(req *context.Request) (err error) {
 	return
 }
 
-func (c *Conn) ReadLine() (buff []byte, err error) {
-	buff, err = c.Reader.ReadBytes(BufferEndFlag)
-	if err == io.EOF {
-		c.closeFlag = true
-	}
-	return buff, err
+func (c *Conn) WriteRequest(request *context.Request) (err error) {
+
+
+
+	return
 }
 
-func (c *Conn) Read() (response *context.Response, err error) {
-	respBytes, err := c.ReadLine()
+func (c *Conn) ReadRequest() (request *context.Request, err error) {
+	buff, err := c.read()
+}
+
+func (c *Conn) Read() (request *context.Request, err error) {
+	reqBytes, err := c.read()
 	if err != nil {
 		err = errors.Trace(err)
 		return
 	}
 
-	response = &context.Response{}
-	if err = json.Unmarshal(respBytes, response); err != nil {
+	request = &context.Request{}
+	if err = json.Unmarshal(reqBytes, request); err != nil {
 		err = errors.Trace(err)
 		return
 	}
@@ -89,14 +116,7 @@ func (c *Conn) Send(req *context.Request) (err error) {
 		return
 	}
 	ctx.SetResponse(resp)
-	c.pushContext(ctx)
 	return
-}
-
-func (c *Conn) pushContext(ctx *context.Context) {
-	go func() {
-		c.ContextChan <- ctx
-	}()
 }
 
 func (c *Conn) Process(handler func(ctx *context.Context)) {
